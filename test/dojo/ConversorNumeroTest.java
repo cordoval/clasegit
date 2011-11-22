8eff6a8b367f37a0f419a5d9655e69cf19219f9d
Junior Corman <junior.corman@gmail.com>
2011-11-22 06:55:00 -0500

    Update test/dojo/ConversorNumeroTest.java

--- test/dojo/ConversorNumeroTest.java
+++ test/dojo/ConversorNumeroTest.java
@@ -17,307 +17,307 @@ public class ConversorNumeroTest {
 
 	@Test
 	public void verificarConversionNumero1() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(1);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(1);
 		assertEquals("I", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero2() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(2);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(2);
 		assertEquals("II", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero3() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(3);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(3);
 		assertEquals("III", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero4() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(4);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(4);
 		assertEquals("IV", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero5() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(5);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(5);
 		assertEquals("V", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero6() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(6);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(6);
 		assertEquals("VI", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero7() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(7);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(7);
 		assertEquals("VII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero8() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(8);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(8);
 		assertEquals("VIII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero9() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(9);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(9);
 		assertEquals("IX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero10() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(10);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(10);
 		assertEquals("X", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero11() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(11);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(11);
 		assertEquals("XI", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero12() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(12);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(12);
 		assertEquals("XII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero13() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(13);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(13);
 		assertEquals("XIII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero14() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(14);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(14);
 		assertEquals("XIV", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero15() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(15);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(15);
 		assertEquals("XV", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero16() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(16);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(16);
 		assertEquals("XVI", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero17() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(17);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(17);
 		assertEquals("XVII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero18() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(18);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(18);
 		assertEquals("XVIII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero19() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(19);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(19);
 		assertEquals("XIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero20() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(20);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(20);
 		assertEquals("XX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero21() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(21);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(21);
 		assertEquals("XXI", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero22() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(22);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(22);
 		assertEquals("XXII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero23() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(23);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(23);
 		assertEquals("XXIII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero24() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(24);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(24);
 		assertEquals("XXIV", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero25() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(25);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(25);
 		assertEquals("XXV", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero26() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(26);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(26);
 		assertEquals("XXVI", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero27() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(27);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(27);
 		assertEquals("XXVII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero28() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(28);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(28);
 		assertEquals("XXVIII", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero29() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(29);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(29);
 		assertEquals("XXIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero30() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(30);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(30);
 		assertEquals("XXX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero35() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(35);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(35);
 		assertEquals("XXXV", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero39() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(39);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(39);
 		assertEquals("XXXIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero40() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(40);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(40);
 		assertEquals("XL", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero50() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(50);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(50);
 		assertEquals("L", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero51() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(51);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(51);
 		assertEquals("LI", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero59() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(59);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(59);
 		assertEquals("LIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero74() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(74);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(74);
 		assertEquals("LXXIV", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero79() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(79);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(79);
 		assertEquals("LXXIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero89() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(89);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(89);
 		assertEquals("LXXXIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero90() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(90);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(90);
 		assertEquals("XC", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero99() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(99);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(99);
 		assertEquals("XCIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero100() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(100);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(100);
 		assertEquals("C", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero101() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(101);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(101);
 		assertEquals("CI", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero109() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(109);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(109);
 		assertEquals("CIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero399() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(399);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(399);
 		assertEquals("CCCXCIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero899() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(899);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(899);
 		assertEquals("DCCCXCIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero989() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(989);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(989);
 		assertEquals("CMLXXXIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero999() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(999);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(999);
 		assertEquals("CMXCIX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero1000() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(1000);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(1000);
 		assertEquals("M", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero1010() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(1010);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(1010);
 		assertEquals("MX", resultadoConversion);
 	}
 
 	@Test
 	public void verificarConversionNumero1050() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(1050);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(1050);
 		assertEquals("ML", resultadoConversion);
 	}
 
@@ -325,7 +325,7 @@ public class ConversorNumeroTest {
 	// romanos y este simbolo es necesario para formar el 4000
 	@Test
 	public void verificarConversionNumero3999() {
-		resultadoConversion = conversor.convierteNumeroDecimalToRomano(3999);
+		resultadoConversion = conversor.convertirNumeroDecimalToRomano(3999);
 		assertEquals("MMMCMXCIX", resultadoConversion);
 	}
 
