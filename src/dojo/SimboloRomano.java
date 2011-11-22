0f1098864a1fa376d5998ac9c74dd1485746e806
Junior Corman <junior.corman@gmail.com>
2011-11-22 06:53:11 -0500

    Update src/dojo/SimboloRomano.java

--- src/dojo/SimboloRomano.java
+++ src/dojo/SimboloRomano.java
@@ -1,5 +1,8 @@
 package dojo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum SimboloRomano {
 
 	UNO("I"), CINCO("V"), DIEZ("X"), CINCUENTA("L"), CIEN("C"), QUINIENTOS("D"), MIL(
@@ -13,6 +16,22 @@ public enum SimboloRomano {
 
 	public String getRepresentacionRomana() {
 		return representacionRomana;
+	}
+
+	public static Map<Integer, String[]> obtenerCaracteresRomanos() {
+		Map<Integer, String[]> caracteresRomanos = new HashMap<Integer, String[]>();
+		String[] unidades = { UNO.representacionRomana,
+				CINCO.representacionRomana, DIEZ.representacionRomana };
+		String[] decenas = { DIEZ.representacionRomana,
+				CINCUENTA.representacionRomana, CIEN.representacionRomana };
+		String[] centenas = { CIEN.representacionRomana,
+				QUINIENTOS.representacionRomana, MIL.representacionRomana };
+		String[] millares = { MIL.representacionRomana, "", "" };
+		caracteresRomanos.put(0, unidades);
+		caracteresRomanos.put(1, decenas);
+		caracteresRomanos.put(2, centenas);
+		caracteresRomanos.put(3, millares);
+		return caracteresRomanos;
 	}
 
 }
