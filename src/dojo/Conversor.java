93fd14f74298d9cfa3eb4f1ab8c31bdf562dd0c7
Junior Corman <junior.corman@gmail.com>
2011-12-07 10:55:50 -0500

    Update

--- src/dojo/Conversor.java
+++ src/dojo/Conversor.java
@@ -5,20 +5,17 @@ import java.util.Stack;
 
 public class Conversor {
 
-	Map<Integer, String[]> caracteresRomanos = SimboloRomano
-			.obtenerCaracteresRomanos();
+	Map<Integer, String[]> caracteresRomanos = SimboloRomano.obtenerCaracteresRomanos();
 
 	public String convertirNumeroDecimalToRomano(int valorConvertir) {
 		String resultadoFinal = "";
 
-		String cadenaInvertida = new StringBuilder().append(valorConvertir)
-				.reverse().toString();
+		String cadenaInvertida = new StringBuilder().append(valorConvertir).reverse().toString();
 
 		Stack<String> resultadoPreliminarPorPosicion = new Stack<String>();
 
 		for (int i = 0; i < cadenaInvertida.length(); i++)
-			resultadoPreliminarPorPosicion.push(obtenerCaracterRomanoSegunPosicion(
-					cadenaInvertida.charAt(i), i));
+			resultadoPreliminarPorPosicion.push(obtenerCaracterRomanoSegunPosicion(cadenaInvertida.charAt(i), i));
 
 		while (!resultadoPreliminarPorPosicion.empty())
 			resultadoFinal += resultadoPreliminarPorPosicion.pop();
@@ -31,61 +28,47 @@ public class Conversor {
 
 		int valorNumerico = new Integer(valor.toString());
 
-		resultado = escribeRango1To9(valorNumerico,
-				caracteresRomanos.get(posicion)[0],
-				caracteresRomanos.get(posicion)[1],
-				caracteresRomanos.get(posicion)[2]);
+		resultado = escribeRango1To9(valorNumerico, caracteresRomanos.get(posicion)[0], caracteresRomanos.get(posicion)[1], caracteresRomanos.get(posicion)[2]);
 
 		return resultado;
 	}
 
-	private String escribeRango1To9(int valor, String romanoInferior,
-			String romanoMedio, String romanoSuperior) {
+	private String escribeRango1To9(int valor, String romanoInferior, String romanoMedio, String romanoSuperior) {
 		String resultado = "";
 		if (valor >= 1 && valor <= 3)
 			resultado = escribirPatronQueRepiteCada3(valor, romanoInferior);
 		else if (valor >= 4 && valor <= 8)
-			resultado = escribirPatronQueRepiteCada5(valor, 5, romanoMedio,
-					romanoInferior);
+			resultado = escribirPatronQueRepiteCada5(valor, 5, romanoMedio, romanoInferior);
 		else if (valor == 9)
-			resultado = escribirPatronQueRepiteCada5(valor, 10, romanoSuperior,
-					romanoInferior);
+			resultado = escribirPatronQueRepiteCada5(valor, 10, romanoSuperior, romanoInferior);
 		return resultado;
 	}
 
-	private String escribirPatronQueRepiteCada3(int repeticiones,
-			String simboloRomano) {
+	private String escribirPatronQueRepiteCada3(int repeticiones, String simboloRomano) {
 		String resultado = "";
 		for (int i = 1; i <= repeticiones; i++)
 			resultado += simboloRomano;
 		return resultado;
 	}
 
-	private String escribirPatronQueRepiteCada5(int valor, int valorBase,
-			String simboloRomanoBase, String simboloRomanoLateral) {
+	private String escribirPatronQueRepiteCada5(int valor, int valorBase, String simboloRomanoBase, String simboloRomanoLateral) {
 		String resultado = "";
 		int diferenciaLateral = valor - valorBase;
 
-		resultado = cuerpoRepetido(diferenciaLateral,
-				simboloRomanoBase, simboloRomanoLateral);
+		resultado = cuerpoRepetido(diferenciaLateral, simboloRomanoBase, simboloRomanoLateral);
 
 		return resultado;
 	}
 
-	private String cuerpoRepetido(int diferenciaLateral,
-			String simboloRomanoBase, String simboloRomanoLateral) {
+	private String cuerpoRepetido(int diferenciaLateral, String simboloRomanoBase, String simboloRomanoLateral) {
 		String resultado = "";
 
 		if (diferenciaLateral < 0) {
-			resultado = escribirPatronQueRepiteCada3(
-					Math.abs(diferenciaLateral), simboloRomanoLateral)
-					+ simboloRomanoBase;
+			resultado = escribirPatronQueRepiteCada3(Math.abs(diferenciaLateral), simboloRomanoLateral) + simboloRomanoBase;
 		} else if (diferenciaLateral == 0) {
 			resultado = simboloRomanoBase;
 		} else {
-			resultado = simboloRomanoBase
-					+ escribirPatronQueRepiteCada3(Math.abs(diferenciaLateral),
-							simboloRomanoLateral);
+			resultado = simboloRomanoBase + escribirPatronQueRepiteCada3(Math.abs(diferenciaLateral), simboloRomanoLateral);
 		}
 		return resultado;
 	}
